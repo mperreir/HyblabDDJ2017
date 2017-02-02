70fb1e9475e25b522a5c1ccd6ea8e7e9efc025e9
Argann Bonneau <argann.b@hotmail.fr>
2017-02-02 09:32:09 +0100

    Remise en forme

--- saint_nazaire/public/js/app.js
+++ saint_nazaire/public/js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+$(document).foundation();
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
