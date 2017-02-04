e85e4b4aa5c2ca0920d16be4c86cb5382829ca92
Kevin BRIAND <kevin.briand2@etu.univ-nantes.fr>
2017-02-04 17:51:33 +0100

    partie 1 quasi finie reste le chart JS

--- saint_nazaire/public/js/impots.js
+++ saint_nazaire/public/js/impots.js
@@ -1,19 +1,39 @@
 "use strict";
 
+// global variables
+var BUBBLE_ANIM_IN = "animated zoomIn";
+var BUBBLE_ANIM_OUT = "animated zoomOut";
+var BUBBLE_TIME_IN = 1200;
+var BUBBLE_TIME_OUT = 800;
+
+
+
 $( document ).ready(function() {
 	
-	// hiding next sections
-	$('#section1_gender').hide();
+	// hiding sections
+	$('#section1_intro').hide();
 	$('#section1_age').hide();
 	$('#section1_residence').hide();
 	$('#section2').hide();
 
 	// hide bubbles
+	$('#bubble1').hide();
 	$('#bubble2').hide();
 	$('#bubble3').hide();
 	$('#bubble4').hide();
 	$('#bubble5').hide();
 	$('#bubble6').hide();
+	$('#bubble7').hide();
+	$('#bubble8').hide();
+	$('#bubble9').hide();
+	$('#bubble10').hide();
+	$('#bubble11').hide();
+	$('#bubble12').hide();
+	$('#bubble13').hide();
+	$('#bubble14').hide();
+	$('#bubble15').hide();
+	$('#bubble16').hide();
+
 
 
 	// select either male or female image
@@ -34,9 +54,23 @@ $( document ).ready(function() {
 
 
 	// slow scroll to selected section
-	$('#toSection1_gender').click(function() { 
-		scroll( $('#section1_intro') , $('#section1_gender') );  
+	$('#toSection1_age').click(function() { 
+		scroll( $('#section1_intro') , $('#section1_age') );  
 	});
+
+
+
+	$('#toSection1_intro').click(function() {
+
+		// getting src image attribute
+		var src = $('.section1_gender_img').attr('src');
+
+		// if src si not defined we do not continue
+		if (src != undefined) { 
+			scroll( $('#section1_gender') , $('#section1_intro') ); 
+		}
+	});
+
 
 
 	$('#toSection1_age').click(function() {
@@ -67,7 +101,10 @@ $( document ).ready(function() {
 		var choice = $('input[name=residence]:checked').val();
 
 		// if the user didn't select yes nor no we do not continue
-		if (choice != undefined) { scroll( $('#section1_residence') , $('#section2') ); }
+		if (choice != undefined) { 
+			scroll( $('#section1_residence') , $('#section2') ); 
+			$('#bubble1').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+		}
 
 	});
 
@@ -75,10 +112,10 @@ $( document ).ready(function() {
 	$('#nextBubble').click(function() {
 		
 		// number of the next bubble
-		var next = parseInt($('#currentBubble').val()) + 1;
+		var currentBubble = parseInt($('#currentBubble').val());
 
 		// display new bubble
-		changeBubble(next);
+		changeBubbleTo(currentBubble);
 	});
 
 
@@ -88,18 +125,66 @@ $( document ).ready(function() {
 
 function scroll(from, to) {
 	from.addClass("animated slideOutRight");
-	from.hide(1000);
+	from.hide(1002);
 	to.show(400);
 	to.addClass("animated slideInUp");
 }
 
-function changeBubble(no) {
+function changeBubbleTo(no) {
 
-	var oldBubble = $('#bubble' + (no - 1));
-	var newBubble = $('#bubble' + no);
-	
-	oldBubble.removeClass("animated zoomIn").addClass("animated zoomOut").hide(1000);
-	newBubble.show().addClass("animated zoomIn");
-	
-	$('#currentBubble').val(no);
+	// hidding and showing the bubbles 
+	switch(no) {
+
+	case 3:
+		$('#bubble3').removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+		$('#bubble4, #bubble5').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+		$('#currentBubble').val(5);
+		break;
+
+	case 5:
+		$('#bubble4, #bubble5').removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+		$('#bubble6, #bubble7').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+		$('#currentBubble').val(7);
+		break;
+
+	case 7:
+		// cost of the user taxes
+		var taxesCost = $('#taxesCost').val();
+
+		// if no answer or illegal answer nothing happend
+		if (taxesCost != "" && parseInt(taxesCost) > 0) { 
+			$('#bubble6, #bubble7').removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+			$('#bubble8').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+			$('#currentBubble').val(8);    	
+		}
+
+		break;
+
+
+	case 13:
+		$('#bubble13').removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+		$('#bubble14, #bubble15').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+		$('#currentBubble').val(15);
+		break;
+
+	case 15:
+		// getting radio button choice (yes/no)
+		var choice = $('input[name=projectsPart]:checked').val();
+
+		// if the user didn't select yes nor no we do not continue
+		if (choice != undefined) { 
+			$('#bubble14, #bubble15').removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+			$('#bubble16').show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+			$('#currentBubble').val(16);
+		}
+		
+		break;
+
+	default:
+		var oldBubble = $('#bubble' + no);
+		var newBubble = $('#bubble' + (no + 1));
+		oldBubble.removeClass(BUBBLE_ANIM_IN).addClass(BUBBLE_ANIM_OUT).hide(BUBBLE_TIME_OUT);
+		newBubble.show(BUBBLE_TIME_IN).addClass(BUBBLE_ANIM_IN);
+		$('#currentBubble').val(no+1);
+	}
 }
