2f425902b622d39deb504ee195ff2b57dae3f437
Argann Bonneau <argann.b@hotmail.fr>
2017-02-04 14:27:50 +0100

    Commit du samedi matin

--- saint_nazaire/server.js
+++ saint_nazaire/server.js
@@ -9,7 +9,11 @@ var twig = require('twig');
 var app = express();
 var fs = require("fs");
 var sqlite3 = require("sqlite3").verbose();
-
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 app.set("twig options", {
     strict_variables: false
 });
@@ -24,10 +28,30 @@ var exists = fs.existsSync(quizdb_name);
 // Database init
 var db = new sqlite3.Database(quizdb_name);
 db.serialize(function(){
-    db.run("CREATE TABLE IF NOT EXISTS REPONSES (" +
+    db.run("PRAGMA foreign_keys = ON;");
+    db.run("CREATE TABLE IF NOT EXISTS UTILISATEURS (" +
+        "ID INT PRIMARY KEY NOT NULL," +
+        "SEXE BOOLEAN NOT NULL," +
+        "AGE INT NOT NULL," +
+        "STNAZAIRE BOOLEAN NOT NULL" +
+        ")");
+    db.run("CREATE TABLE IF NOT EXISTS PROJETS (" +
+        "ID INT PRIMARY KEY NOT NULL," +
+        "NOM TEXT(60) NOT NULL" +
+        ")");
+    db.run("CREATE TABLE IF NOT EXISTS ESTIMATION_PROJET (" +
         "ID_PROJET INT NOT NULL," +
+        "ID_UTILISATEUR INT NOT NULL," +
         "ESTIMATION INT NOT NULL," +
-        "NOTE INT NOT NULL" +
+        "FOREIGN KEY(ID_PROJET) REFERENCES PROJETS(ID)," +
+        "FOREIGN KEY(ID_UTILISATEUR) REFERENCES UTILISATEURS(ID)" +
+        ")");
+    db.run("CREATE TABLE IF NOT EXISTS NOTE_PROJET (" +
+        "ID_PROJET INT NOT NULL," +
+        "ID_UTILISATEUR INT NOT NULL," +
+        "NOTE INT NOT NULL," +
+        "FOREIGN KEY(ID_PROJET) REFERENCES PROJETS(ID)," +
+        "FOREIGN KEY(ID_UTILISATEUR) REFERENCES UTILISATEURS(ID)" +
         ")");
 })
 db.close();
@@ -42,9 +66,59 @@ app.use(express.static(path.join(__dirname, 'public')));
  *  Route to Quiz
  */
 app.all('/quiz', function(req, res){
+    res.render('quiz.html.twig');
+});
 
-    res.render('index.html.twig');
+/**
+ *  --- BACKEND ---
+ */
+app.get('/projets/get/all', function(req, res){
+    var db = new sqlite3.Database(quizdb_name);
+    db.all("SELECT * FROM PROJETS", function(err, rows){
+        res.json(rows);
+    })
+    db.close();
+});
 
+app.get('/projets/get/:id', function(req, res){
+    var db = new sqlite3.Database(quizdb_name);
+    db.all("SELECT * FROM PROJETS WHERE ID = ?", [req.params.id], function(err, rows){
+        res.json(rows);
+    })
+    db.close();
+})
+
+app.get("/utilisateurs/get/all", function(req, res){
+    var db = new sqlite3.Database(quizdb_name);
+    db.all("SELECT * FROM UTILISATEURS", function(err, rows){
+        res.json(rows);
+    })
+    db.close();
+});
+
+app.get('/utilisateurs/get/:id', function(req, res){
+    var db = new sqlite3.Database(quizdb_name);
+    db.all("SELECT * FROM UTILISATEURS WHERE ID = ?", [req.params.id], function(err, rows){
+        res.json(rows);
+    })
+    db.close();
+})
+
+app.get('/projets/add', function(req, res){
+    var db = new sqlite3.Database(quizdb_name);
+    var lastid = 0;
+    // TODO REPARER CETTE MERDE !!!!!
+    db.serialize(function(){
+        var stmt = db.prepare("INSERT INTO PROJETS (NOM) VALUES (?)");
+        stmt.run(req.query.nom);
+        stmt.finalize();
+
+        db.get("SELECT * FROM PROJETS ORDER BY ID DESC LIMIT 1", function(err, row){
+            lastid = row.ID;
+            res.json({'lastid': lastid});
+        });
+    });
+    db.close();
 });
 
 /**
